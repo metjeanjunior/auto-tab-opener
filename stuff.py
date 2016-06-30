01cfcb859d61e006dc86ffa94c3b2eb2e9079128
metjeanjunior <metjeanjunior@gmail.com>
2016-06-30 14:07:39 -0400

    can open links

---
+++ stuff.py
@@ -0,0 +1,22 @@
+import webbrowser
+
+url_list = ()
+always_open_cmd = True
+always_load_url_list = True
+
+if always_open_cmd:
+	url_list = () if not always_load_url_list else url_list
+	print 'Please type the links you\'d like to open.'
+
+	while True:
+		print 'You can either enter a new link or type done.'
+		res = raw_input('>>>')
+
+		if res == 'done':
+			break
+		url_list += (res, )
+		print 'added %s to url_list' % res
+		print 'your list is now: ', url_list[0]
+
+for url in url_list:
+	webbrowser.open(url)
